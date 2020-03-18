89bd7e87215ec3c4bf73615b54a06ddce86ed440
Stanley Szeto <sszeto@uwaterloo.ca>
2020-03-18 19:09:20 -0400

    updated instructions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Hot Module Replacement
 if (module.hot) {
     module.hot.accept();
-    module.hot.dispose(() => console.log('Module disposed. '));
+    module.hot.dispose(() => console.log('change this to server.stop() for apollo'));
 }
